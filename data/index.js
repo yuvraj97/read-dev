4fd74d5e202eb1ac54b18b28112fbc4d11bad629
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-11-21 03:35:46 +0530

    1

---
+++ data/index.js
@@ -0,0 +1,83 @@
+// Change Theme
+const changeTheme = document.getElementById('change-theme');
+changeTheme.addEventListener('click',(e) => {    
+	e.preventDefault();
+	window.location.reload();
+	var cssFiles = document.getElementsByTagName("link");
+	main_css = "";
+    for (var i=0; i<cssFiles	.length; i++){
+		if(cssFiles[i].getAttribute("href").search("main")!=-1){
+			main_css = cssFiles[i];
+		}
+	}
+	href = main_css.href;
+	
+	currTheme = localStorage.getItem("quantmlTheme");
+	if(currTheme == "light"){
+		console.log("Light2Dark");
+		localStorage.setItem("quantmlTheme", "dark");
+		href = href.replace("main.css", "main-dark.css");
+		changeTheme.src = changeTheme.src.replace("/img/","/img-dark/");
+	}
+	else if (currTheme == "dark"){
+		console.log("Dark2Light");
+		localStorage.setItem("quantmlTheme", "light");
+		href = href.replace("main-dark.css", "main.css");
+		changeTheme.src = changeTheme.src.replace("/img-dark/","/img/");
+	}
+	main_css.href = href;
+	
+});
+
+// Set Theme icon
+currentTheme = localStorage.getItem("quantmlTheme");
+if(currentTheme == "light"){
+	changeTheme.src = "/data/img/change-theme.png"
+}
+if(currentTheme == "dark"){
+	changeTheme.src = "/data/img-dark/change-theme.png"
+}
+
+
+// Set images according to Theme
+images = document.querySelectorAll('img')
+if(currentTheme == "light"){
+	for (let index = 0; index < images.length; index++) {
+		// console.log(images[index].src,"->", images[index].src.replace("/img-dark/","/img/"))
+		images[index].src = images[index].src.replace("/img-dark/","/img/")
+	}
+} else if(currentTheme == "dark"){
+	for (let index = 0; index < images.length; index++) {
+		// console.log(images[index].src,"->", images[index].src.replace("/img/","/img-dark/"))
+		images[index].src = images[index].src.replace("/img/","/img-dark/")
+	}
+}
+
+function coverLocation(){
+	if(currentTheme == "light"){
+		return "/data/img/cover.png"
+	} else if(currentTheme == "dark"){
+		return "/data/img-dark/cover.png"
+	}	
+}
+
+
+// Set Next-Prev btn width
+setTimeout (()=>{
+	prev = document.querySelectorAll('#prev-btn')
+	next = document.querySelectorAll('#next-btn')
+	btnWidth = document.getElementById('btn-container').offsetWidth
+	if(prev.length != 0 && prev[0].offsetWidth + next[0].offsetWidth > btnWidth) {
+		prev[0].style.display="block";
+		prev[1].style.display="block";
+		next[0].style.display="block";
+		next[1].style.display="block";
+		prev[0].style.width="100%";
+		prev[1].style.width="100%";
+		next[0].style.width="100%";
+		next[1].style.width="100%";
+		next[0].style.marginTop="5px"
+		next[1].style.marginTop="5px"
+		
+	}
+}, 100)
---
+++ data/initialize-css.js
@@ -0,0 +1,14 @@
+if(localStorage.getItem("quantmlTheme")==null){
+	localStorage.setItem("quantmlTheme", "light");
+}
+currTheme = localStorage.getItem("quantmlTheme")
+theme = document.getElementById('quantml-theme')
+auththeme = document.getElementById('quantml-auth-style')
+if(currTheme == "light"){
+	theme.href ="/data/assets/css/main.css"
+	auththeme.href = "/data/auth/authStyle.css"
+}
+else if(currTheme == "dark") {
+	theme.href = "/data/assets/css/main-dark.css"
+	auththeme.href = "/data/auth/authStyle-dark.css"
+}
