c44d1e2c6dad0d3db91147ee953a56885c1e050d
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-12-23 03:22:26 +0530

    clt/index.html

--- data/assets/js/script.js
+++ data/assets/js/script.js
@@ -272,8 +272,8 @@ function paginationButton(){
 	if(typeof(nextPage) !="undefined" && typeof(prevPage) !="undefined" ){
 		btnContainer.forEach(function(element, index){
 			element.innerHTML=`	
-			<a href="${prevPage}"><button id="prev-btn" class="button">&#x25C0;&nbsp;&nbsp; Introduction</button></a>
-			<a href="${nextPage}"><button id="next-btn" class="button"  style="float: right;">Central Limit Theorem&nbsp;&nbsp;&#x25B6;</button></a>
+			<a href="${prevPage}"><button id="prev-btn" class="button">&#x25C0;&nbsp;&nbsp; ${prevPageTitle}</button></a>
+			<a href="${nextPage}"><button id="next-btn" class="button"  style="float: right;">${nextPageTitle}&nbsp;&nbsp;&#x25B6;</button></a>
 			`
 		})
 	}
