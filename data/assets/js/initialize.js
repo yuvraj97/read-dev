4d7b6f33ca7798b965c53ab5496683a22f0fe171
Yuvraj Garg <yuvraj97.ml@gmail.com>
2020-12-30 04:34:05 +0530

    getCookie()

--- data/assets/js/initialize.js
+++ data/assets/js/initialize.js
@@ -85,6 +85,17 @@ function requireScript(name, version, url, callback) {
   }
 }
 
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 // Setting Theme
 if(localStorage.getItem("quantmlTheme")==null){
 	localStorage.setItem("quantmlTheme", "light");
