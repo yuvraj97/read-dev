9dd3524423d8b65b4a28aafca1e72b3e7c5ae917
Yuvraj Garg <yuvraj97.ml@gmail.com>
2021-02-12 09:50:05 +0530

    Update py.py

--- py.py
+++ py.py
@@ -12,14 +12,34 @@ from urllib.parse import urlencode
 import sys
 from shutil import copyfile
 
+skip_files = ["py.py", "requirements.txt", ".gitignore"]
+skip_folders = ["tobeadded", "staged", ".vscode", ".git"]
+remove_files = ["initialize.js", "inline.css",
+                # Don't Count    
+                "style.css", "style-dark.css", "style-light.css", "TODO.txt"
+               ]
+files_without_ext = ["_redirects"]
+do_not_print_ext = ["png", "webp", "svg", "html", "woff2", "woff", "ttf"]
+
+def ext_in_do_not_print_ext(f):
+    if("." not in f): return True
+    for ext in do_not_print_ext:
+        if ext in f:
+            return True
+    return False
+
 def get_files_loc(curr_loc, d):
     for f in os.listdir(curr_loc):
-        if f == ".git":
+        if not ext_in_do_not_print_ext(f): print(f'"{f}"')
+        if f in skip_folders or f in skip_files or f in remove_files:
+            if not ext_in_do_not_print_ext(f): print("\t continue")
             continue
-        if '.' not in f and f != "_redirects":
+        if '.' not in f and f not in files_without_ext:
+            if not ext_in_do_not_print_ext(f): print("\t '.' not in f and f not in files_without_ext")
             get_files_loc(os.path.join(curr_loc, f), d)
         else:
             splited = f.split('.')
+            if not ext_in_do_not_print_ext(f): print("\t else")
             if("css" in splited and "min" not in splited):
                 d["css"].append(os.path.join(curr_loc, f))
             # elif("js" in splited):
@@ -38,8 +58,11 @@ get_files_loc(os.getcwd(), origin)
 failed_html = []
 for html_file in origin["html"]:
     try:
-        f = open(html_file, "r")
+        f = open(html_file, "r")#, encoding="utf8")
         html = f.read()
+        html = html.replace("% equation","")
+        html = html.replace("%equation","")
+        html = html.replace("%blank line","")
         f.close()
         
         minified = htmlmin.minify(html, 
